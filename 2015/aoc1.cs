fc0c0265af080749449ead5074a11d33068e2f9f
UtahSTEMandArtsAcademy <153949503+UtahSTEMandArtsAcademy@users.noreply.github.com>
2024-07-11 17:08:04 -0600

    aoc day 5 part 2

---
+++ 2015/aoc1.cs
@@ -0,0 +1,8 @@
+string inputs = "(())";
+int floor = 0;
+for(int i = 0; i < inputs.Length; i++ )
+{
+    char cache = inputs[i];
+    Console.WriteLine(i+": "+cache);
+    
+}
--- obj/Debug/net8.0/duncan.AssemblyInfo.cs
+++ obj/Debug/net8.0/duncan.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("duncan")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b00735362110a0fd5cf069b46e41a4959f6728d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42dc025698f2763a2e6bad4cb1d6c5cb361e9ba2")]
 [assembly: System.Reflection.AssemblyProductAttribute("duncan")]
 [assembly: System.Reflection.AssemblyTitleAttribute("duncan")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
